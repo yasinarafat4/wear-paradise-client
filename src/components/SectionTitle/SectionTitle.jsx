84a453868d7d56fbe9c86cfe2960b3e41920fde3
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-07-08 15:27:59 +0600

    products.json file added

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 const SectionTitle = ({ heading }) => {
   return (
-    <div className="text-center space-y-4 my-6 md:my-16 w-9/12 md:w-7/12 lg:w-5/12 mx-auto">
-      <h2 className="text-xl text-[#e84766] md:text-3xl font-semibold py-2 md:py-4">
+    <div className="text-center space-y-4 mt-6 mb-3 md:mt-16 md:mb-5 w-9/12 md:w-7/12 lg:w-5/12 mx-auto">
+      <h2 className="text-xl text-[#e84766] md:text-3xl lg:text-4xl font-semibold py-2 md:py-4">
         {heading}
       </h2>
     </div>
