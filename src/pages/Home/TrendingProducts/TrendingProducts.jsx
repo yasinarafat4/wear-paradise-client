0fe1c9c0fb41c6c26f80c2e7bd2d1674aa01338d
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-07-09 14:50:40 +0600

    Custom hook for products has added

--- src/pages/Home/TrendingProducts/TrendingProducts.jsx
+++ src/pages/Home/TrendingProducts/TrendingProducts.jsx
@@ -1,9 +1,20 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useProducts from "../../../hooks/useProducts";
 
 const TrendingProducts = () => {
+  const [products] = useProducts();
+  const trending = products.filter((item) => item.category === "trending");
   return (
     <div>
       <SectionTitle heading={"Trending Items"}></SectionTitle>
+
+      {trending.map((product) => (
+        <>
+          <p>{product._id}</p>
+          <p key={product._id}>{product.product_name}</p>
+          <img src={product.product_image} alt="" />
+        </>
+      ))}
     </div>
   );
 };
