d15d76612f43391b9f88be2798096f2c93c70449
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-07-09 19:35:31 +0600

    trending items section added with React Fast Marquee

---
+++ src/components/TrendingCard/TrendingCard.jsx
@@ -0,0 +1,32 @@
+import { FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
+
+const TrendingCard = ({ product }) => {
+  const { product_image, product_name, price, rating } = product;
+  return (
+    <div className="m-4 shadow-lg p-4 cursor-pointer">
+      <img
+        className="w-5/12 md:w-6/12 lg:w-7/12 h-3/5  md:h-4/6 rounded-md mx-auto mb-3"
+        src={product_image}
+        alt=""
+      />
+      <div className="space-y-1">
+        <p className="font-medium">{product_name}</p>
+        <p>
+          <span className="font-medium">Price:</span> ${price}
+        </p>
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating}
+            emptySymbol={<FaRegStar className="text-yellow-400"></FaRegStar>}
+            readonly
+            placeholderSymbol={<FaStar className="text-yellow-400"></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingCard;
--- src/pages/Home/TrendingProducts/TrendingProducts.jsx
+++ src/pages/Home/TrendingProducts/TrendingProducts.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import TrendingCard from "../../../components/TrendingCard/TrendingCard";
 import useProducts from "../../../hooks/useProducts";
+import Marquee from "react-fast-marquee";
 
 const TrendingProducts = () => {
+  // push Marquee when Mouse Over
+  const [isPaused, setIsPaused] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsPaused(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsPaused(false);
+  };
+
   const [products] = useProducts();
   const trending = products.filter((item) => item.category === "trending");
   return (
-    <div>
+    <>
       <SectionTitle heading={"Trending Items"}></SectionTitle>
-
-      {trending.map((product) => (
-        <>
-          <p>{product._id}</p>
-          <p key={product._id}>{product.product_name}</p>
-          <img src={product.product_image} alt="" />
-        </>
-      ))}
-    </div>
+      <Marquee
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        speed={60}
+        gradient={false}
+        pauseOnHover={!isPaused}
+      >
+        <div className="grid grid-cols-5 mx-4">
+          {trending.map((product) => (
+            <TrendingCard key={product._id} product={product}></TrendingCard>
+          ))}
+        </div>
+      </Marquee>
+    </>
   );
 };
 
