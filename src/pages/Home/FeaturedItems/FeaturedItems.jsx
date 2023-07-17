cd9204cb090efdb1138d22bcbda50869d5cf79eb
Yasin <mdyasinarafathasib@gmail.com>
2023-07-17 12:51:23 +0600

    Reviews added

--- src/pages/Home/FeaturedItems/FeaturedItems.jsx
+++ src/pages/Home/FeaturedItems/FeaturedItems.jsx
@@ -21,7 +21,7 @@ const FeaturedItems = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 3000,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -31,47 +31,85 @@ const FeaturedItems = () => {
         modules={[Autoplay, Pagination, Navigation]}
       >
         <SwiperSlide>
-          <div className="md:grid grid-cols-2 justify-center items-center bg-gradient-to-r from-red-400 to-purple-700 py-8">
-            <img src="https://i.ibb.co/Mgwtmvh/img1-2.png" alt="" data-aos="fade-right" data-aos-duration="1000"/>
+          <div className="md:grid grid-cols-2 justify-center items-center bg-gradient-to-r from-red-400 to-purple-700 py-8 md:py-12 lg:py-9">
+            <div className="relative">
+              <div className="absolute top-3 md:top-1 left-16 lg:left-20 xl:left-40 badge bg-red-600 border-0 text-white rounded-full py-7 px-3 lg:py-10 lg:px-5 flex flex-col animate-sway" data-aos="fade-left"
+                data-aos-duration="1000">
+                <p className="text-base lg:text-xl font-semibold tracking-[.4px]">57%</p>
+                <p className="tracking-[.4px]">Offer</p>
+              </div>
+              <img
+                src="https://i.ibb.co/Mgwtmvh/img1-2.png"
+                alt=""
+                data-aos="fade-right"
+                data-aos-duration="1000"
+              />
+            </div>
             <div className="ps-4 space-y-6">
-              <h2 className="text-2xl md:text-4xl text-white font-bold w-10/12 text-start" data-aos="fade-up" data-aos-duration="1000">
+              <h2
+                className="text-2xl md:text-4xl text-white font-bold w-10/12 text-start"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 Join Now & Earn 5% Rewads on Every Order.
               </h2>
-              <p className="md:text-xl text-white w-10/12 text-start" data-aos="fade-up" data-aos-duration="1000">
+              <p
+                className="md:text-xl text-white w-10/12 text-start"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 $280.00
               </p>
-              <div className="flex justify-start items-center gap-3 pb-4" data-aos="fade-up" data-aos-duration="1000">
+              <div
+                className="flex justify-start items-center gap-3 pb-4"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 <button className="flex items-center px-4 py-3 md:px-6 md:py-[14px] text-white bg-[#df2c50] hover:bg-[#268ccc] border-0 duration-500">
                   <FaShoppingBasket className="mr-2" />
                   <span>Buy Now</span>
                 </button>
-                <Link to='/products'>
-                <button className="px-4 py-[10px] md:px-6 md:py-3 border-2 text-white bg-black hover:bg-transparent duration-500">
-                  View Products
-                </button></Link>
+                <Link to="/products">
+                  <button className="px-4 py-[10px] md:px-6 md:py-3 border-2 text-white bg-black hover:bg-transparent duration-500">
+                    View Products
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="md:grid grid-cols-2 justify-center items-center bg-gradient-to-r from-blue-300 to-purple-700 pb-14">
-            <img src="https://i.ibb.co/ZYHP12x/img2.png" alt=""/>
-            <div className="ps-4 space-y-6">
-              <h2 className="text-2xl md:text-4xl text-white font-bold w-10/12 text-start" data-aos="fade-up" data-aos-duration="1000">
+          <div className="md:grid grid-cols-2 justify-center items-center bg-gradient-to-r from-blue-300 to-purple-700 pb-20">
+            <img src="https://i.ibb.co/ZYHP12x/img2.png" alt="" />
+            <div className="ps-4 space-y-6 pt-4">
+              <h2
+                className="text-2xl md:text-4xl text-white font-bold w-11/12 lg:w-10/12 text-start"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 Preferable Designed New Nike Tech Pack for Men.
               </h2>
-              <p className="md:text-xl text-white w-10/12 text-start" data-aos="fade-up" data-aos-duration="1000">
+              <p
+                className="md:text-xl text-white w-10/12 text-start"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 $320.00
               </p>
-              <div className="flex justify-start items-center gap-3 pb-4" data-aos="fade-up" data-aos-duration="1000">
+              <div
+                className="flex justify-start items-center gap-3 pb-4"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
                 <button className="flex items-center px-4 py-3 md:px-6 md:py-[14px] text-white bg-[#df2c50] hover:bg-[#b000b9] border-0 duration-500">
                   <FaShoppingBasket className="mr-2" />
                   <span>Buy Now</span>
                 </button>
-                <Link to='/products'>
-                <button className="px-4 py-[10px] md:px-6 md:py-3 border-2 text-white bg-black hover:bg-transparent duration-500">
-                  View Products
-                </button></Link>
+                <Link to="/products">
+                  <button className="px-4 py-[10px] md:px-6 md:py-3 border-2 text-white bg-black hover:bg-transparent duration-500">
+                    View Products
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -1,9 +1,69 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
+// Import for Ratings
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
 const Reviews = () => {
+  const reviews = [
+    {
+      id: 1,
+      name: "Alisa Johnson",
+      image: "https://i.ibb.co/RTwrv9r/consumer1.png",
+      description:
+        "Wear Paradice is an amazing e-commerce website! They offer a wide range of trendy and high-quality clothing, shoes, and accessories. The user-friendly interface makes it easy to browse and find exactly what you're looking for. Their fast shipping and excellent customer service make shopping a breeze. Highly recommended!",
+      rating: 5,
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      image: "https://i.ibb.co/xjY8W77/consumer2.png",
+      description:
+        "I'm absolutely in love with Wear Paradice! They have an incredible selection of fashionable apparel for all occasions. From casual wear to formal attire, they have it all. The website is beautifully designed and provides a seamless shopping experience. The prices are competitive, and the delivery is always prompt. I can't recommend Wear Paradice enough!",
+      rating: 4,
+    },
+    {
+      id: 3,
+      name: "Mark Johnson",
+      image: "https://i.ibb.co/GPqT8gC/consumer3.png",
+      description:
+        "Wear Paradice is my go-to online store for all things fashion. They offer a vast collection of stylish and on-trend clothing items that suit every taste and budget. The website is easy to navigate, and their product descriptions are accurate. The customer support team is friendly and responsive. I've had nothing but positive experiences with Wear Paradice!",
+      rating: 4.5,
+    },
+  ];
+
   return (
     <div>
       <SectionTitle heading={"What Our Consumers Say"}></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]}>
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id}>
+            <div className="text-center flex flex-col items-center space-y-3 md:space-y-6">
+              <div className="w-3/12 2xl:w-1/12 h-[90px] md:h-[185px] lg:h-[255px] xl:h-[330px] 2xl:h-[213px] rounded-full overflow-hidden">
+                <img
+                  className="object-cover"
+                  src={review.image}
+                  alt=""
+                />
+              </div>
+              <h3 className="text-[#b000b9] uppercase text-base md:text-2xl font-semibold">
+                {review.name}
+              </h3>
+              <Rating
+                style={{ maxWidth: 150 }}
+                value={review.rating}
+                readOnly
+              />
+              <p className="text-sm md:text-md lg:text-lg w-8/12 md:w-9/12 lg:w-8/12">{review.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
