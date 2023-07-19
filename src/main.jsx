2c6722bb20e8afe5e633faa753899f1ab5d4aba0
Yasin <mdyasinarafathasib@gmail.com>
2023-07-19 13:01:37 +0600

    Auth COntext added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "./index.css";
-import { router } from "./Routes/Router";
 import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Router";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+
+const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
